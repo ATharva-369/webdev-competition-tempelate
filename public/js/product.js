bfe86272495fd05852653b60dc22b661f917e09b
Kayz_xx <kaydenmoraes@gmail.com>
2022-11-25 18:03:08 +0530

    u

--- public/js/product.js
+++ public/js/product.js
@@ -1,12 +1,151 @@
-import products from '/js/products.js'
- $(document).ready(function () {
-    console.log("js")
-    $(function () {
-       for (var i = 0; i < products.length; i++) {
-          console.log(products[i])
-          $("#card").html(`${products[i].name} <br> ${products[i].price} <br> ${products[i].category} <img class="card-image" src=images/${products[i].image}.png />`)
-          $(".card-img").css({"width":"100px", "height":"100px"})
-       }
+import products from './products'
+$(document).ready(function () {
+	console.log('js');
+	$(function () {
+		for (var i = 0; i < products.length; i++) {
+			console.log(products[i]);
+			$("div."+i).html(
+				`${products[i].name} <br> ${products[i].price} <br> ${products[i].category} <img class="card-image" src=images/${products[i].image}.png />`,
+			);
+         $('#card').append($("div."+i))
 
-    });
- })
+		}
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/js/products.js
+++ public/js/products.js
@@ -1,13 +1,10 @@
-const products = [{
-    "name": "necklace",
-    "price": "300",
-    "category": "jewellery",
-    "image":"logo"
- },
- {"name":"bag",
-"price":"500",
-"category":"accessories",
-"image":"unnamed"}
-
-]
- export default products;
+const products = [
+	{
+		name: 'necklace',
+		price: '300',
+		category: 'jewellery',
+		image: 'logo',
+	},
+	{ name: 'bag', price: '500', category: 'accessories', image: 'unnamed' },
+];
+export default products;
